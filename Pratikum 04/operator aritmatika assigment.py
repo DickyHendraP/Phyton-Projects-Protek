b9508db2101b473b2c03f48cbb913b905ece8833
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-16 19:22:24 +0700

    Create operator aritmatika assigment.py

---
+++ Pratikum 04/operator aritmatika assigment.py
@@ -0,0 +1,58 @@
+a=10
+b=2.5
+#operator aritmatika (+) penjumlahan
+print(type(a+a))
+print(type(a+b))
+print(type(b+a))
+print(type(b+b))
+
+#operator aritmatika (-) pengurangan
+print(type(a-a))
+print(type(a-b))
+print(type(b-a))
+print(type(b-b))
+
+#operator aritmatika (*) perkalian 
+print(type(a*a))
+print(type(a*b))
+print(type(b*a))
+print(type(b*b))
+
+#operator aritmatika (/) pembagian
+print(type(a/a))
+print(type(a/b))
+print(type(b/a))
+print(type(b/b))
+
+#operator aritmatika (//) pembagian dengan pembulatan ke bawah
+print(type(a//a))
+print(type(a//b))
+print(type(b//a))
+print(type(b//b))
+
+#operator aritmatika (%)sisa hasil bagi 
+print(type(a%a))
+print(type(a%b))
+print(type(b%a))
+print(type(b%b))
+
+#operator aritmatika (**) perpangkatan
+print(type(a**a))
+print(type(a**b))
+print(type(b**a))
+print(type(b**b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
