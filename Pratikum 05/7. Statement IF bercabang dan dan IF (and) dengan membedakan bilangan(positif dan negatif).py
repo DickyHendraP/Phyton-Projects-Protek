880a9365c8e3613892b1ec83a621716e9b6e6dd0
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-29 11:56:41 +0700

    Create 7. Statement IF bercabang dan dan IF (and) dengan membedakan bilangan(positif dan negatif).py

---
+++ Pratikum 05/7. Statement IF bercabang dan dan IF (and) dengan membedakan bilangan(positif dan negatif).py
@@ -0,0 +1,19 @@
+a = -8
+b = 3
+if (a > 0) and (b > 0) :
+    print('keduanya Postif')
+else:
+    print('Keduanya tidak Positif')
+
+# dengan program di bawah ini apakah sama
+
+a = 8
+b = -3
+if (a > 0):
+    if (b > 0):
+        print('Keduanya Positif')
+    else :
+        print('Keduanya tidak Positif')
+else :
+    print('Keduanya tidak Positif')
+        
