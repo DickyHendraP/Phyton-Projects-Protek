74fff5466a0f00cd636e5caece5ff4ea658b4c89
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-29 11:50:09 +0700

    Create 6. Statement IF (and or).py

---
+++ Pratikum 05/6. Statement IF (and or).py
@@ -0,0 +1,12 @@
+#Statement IF (and or)
+
+a=-2
+b=-7
+if (a>0)and(b>0):
+    print('Keduanya Positif')
+if (a>0)or(b>0):
+    print('Salah Satu Positif')
+if (a<0)or(b<0):
+    print('Salah Satu Negatif')
+if (a<0)and(b<0):
+    print('Keduanya Negatif')
