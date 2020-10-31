a5e5a6a79184d09a56f013ef36200faac9c94566
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-31 16:19:05 +0700

    Update latihan 2 nomor 2.py

--- Pratikum 05/latihan 2 nomor 2.py
+++ Pratikum 05/latihan 2 nomor 2.py
@@ -7,9 +7,10 @@ i=1
 ##membuat perulangan jika i masih di kurang dari 100 maka akan masuk mencetak i
 #dan i di tambah dengan 2
 #dengan begitu maka yang di cetak adalah angka ganjil semua dari 1 sampai dengan 99
-#variable k di sini berfungsi untuk mengitung berapa banyak jumlah bilangan ganjil
+#variable k di sini berfungsi untuk mengitung berapa banyak jumlah bilangan genap
 while (i <=100):
     print(i) 
     i=i + 2
     k=k+1
+#membuat tampilan output untuk mengetahui bilangan genap ada berapa
 print('Jumlah bilangan ganjil : '+ str(k))
