a631aaa34d24fb11960fa4fa1177890bdd4c02da
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-31 16:14:02 +0700

    Update latihan 2 nomor 1.py

--- Pratikum 05/latihan 2 nomor 1.py
+++ Pratikum 05/latihan 2 nomor 1.py
@@ -1,5 +1,11 @@
 #program untuk menampilkan bilangan bulat dari 0 sampai dengan 100 yang ganjil
+
+#Membuat Variable yang di mulai dari 1
 i=1
+
+#membuat perulangan jika i masih di kurang dari 100 maka akan masuk mencetak i
+#dan i di tambah dengan 2
+#dengan begitu maka yang di cetak adalah angka ganjil semua dari 1 sampai dengan 99
 while (i <=100):
     print(i) 
     i=i + 2
