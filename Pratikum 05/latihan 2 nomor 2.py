6a6d249c1fbf1f2a14c3e3e540c963a1ed29faf5
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-10-31 16:17:53 +0700

    Create latihan 2 nomor 2.py

---
+++ Pratikum 05/latihan 2 nomor 2.py
@@ -0,0 +1,15 @@
+#Modifikasi Program untuk menampilkan bilangan bulat dari 0 sampai dengan 100 yang ganjil
+#bisa menampilkan banyaknya bilangan bulat dari 0 sampai 100 yang ganjil
+
+#Membuat Variable i=1 dan k=0
+k=0
+i=1
+##membuat perulangan jika i masih di kurang dari 100 maka akan masuk mencetak i
+#dan i di tambah dengan 2
+#dengan begitu maka yang di cetak adalah angka ganjil semua dari 1 sampai dengan 99
+#variable k di sini berfungsi untuk mengitung berapa banyak jumlah bilangan ganjil
+while (i <=100):
+    print(i) 
+    i=i + 2
+    k=k+1
+print('Jumlah bilangan ganjil : '+ str(k))
