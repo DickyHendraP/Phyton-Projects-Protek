849608059d6dae4bf8a6e982c07b840cf5311f2a
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-11-05 10:09:55 +0700

    Create 1. Mendefinisikan Function.py

---
+++ Pratikum 06/1. Mendefinisikan Function.py
@@ -0,0 +1,21 @@
+#Materi mendefinisikan Function
+print ('------------------------------------------')
+print ('             ***** HELLO WORLD *****        ')
+print ('------------------------------------------')
+print ('------------------------------------------')
+print ('             ***** HELLO WORLD *****        ')
+print ('------------------------------------------')
+
+#membuat enter/ pemisah program di atas dan di bawah
+print (' ')
+
+#sedangkan dengan program function di bawah ini
+def Membuatprint ():
+    print ('------------------------------------------')
+    print ('             ***** HELLO WORLD *****        ')
+    print ('------------------------------------------')
+Membuatprint ()
+Membuatprint ()
+
+
+    
