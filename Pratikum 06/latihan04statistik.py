39d6798f9d0bb803541c20ceff4162e7b49cc242
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-11-05 10:18:41 +0700

    Create latihan04statistik.py

---
+++ Pratikum 06/latihan04statistik.py
@@ -0,0 +1,34 @@
+#latihan soal 4 materi function
+#stastik
+#sum
+def sum(*n) :
+    i = 0
+    for x in n :
+        i = i + x
+    print (i)
+
+#avarage
+def avarage(*n):
+    i = 0
+    j = 0
+    for x in n :
+        i = i + x
+        j += 1
+    avarage = i/j
+    print (avarage)
+
+#maks
+def max (*n):
+    max = n[0]
+    for i in (n) :
+        if (i>max):
+            max = i
+    print (max)
+
+#min
+def min(*n):
+    min = n[0]
+    for i in (n) :
+        if (i < min):
+            min = i
+    print (min)
