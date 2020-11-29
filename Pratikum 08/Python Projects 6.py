2972e636a846658e1f5c15bb43cfb2af00cb5f14
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-11-30 05:35:42 +0700

    Create Python Projects 6.py

---
+++ Pratikum 08/Python Projects 6.py
@@ -0,0 +1,9 @@
+def sortStringByChar (mydataku) :
+    mydataku.sort(reverse=True)
+    return mydataku
+
+mydataku = ['apel', 'rambutan', 'jeruk']
+dataSorted = sortStringByChar (mydataku)
+
+print ('Data Baru : ', dataSorted)
+    
