9dea896c748f6b3c8b0d29bb4b8b5e2a0822e964
DickyHendraPamungkas <71623032+DickyHendraP@users.noreply.github.com>
2020-12-22 07:06:30 +0700

    Create Nomer 5 Manipulasi String (MENAMPILKAN DATA LIST MAHASISWA).py

---
+++ Pratikum 09/Nomer 5 Manipulasi String (MENAMPILKAN DATA LIST MAHASISWA).py
@@ -0,0 +1,16 @@
+#Menampilkan data List Mahasiswa ke dalam bentuk tabel
+
+nilai = [{'nim' : 'A01', 'nama' : 'Agustina', 'mid' : 50, 'uas' : 80},
+         {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90}, 
+         {'nim' : 'A03', 'nama' : 'Chicha', 'mid' : 100, 'uas' : 50}, 
+         {'nim' : 'A04', 'nama' : 'Donna', 'mid' : 20, 'uas' : 100},
+         {'nim' : 'A05', 'nama' : 'Fatimah', 'mid' : 70, 'uas' : 100}]
+
+print ("=" * 41)
+print("NIM".ljust(10), "NAMA".ljust(10), "N.MID".rjust(10), "N.UAS".rjust(10))
+print("-" * 41)
+
+for x in nilai :
+    print(x['nim'].ljust(10), x['nama'].ljust(10), str(x['mid']).rjust(10), str(x['uas']).rjust(10))  
+
+
